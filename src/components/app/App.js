7518c13dd05748e4ef5724c0d705adabd9cc11d2
Igor <katsuba96@gmail.com>
2022-09-30 17:21:39 +0300

    added adaptive for buttons, added and adapted grow section

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import './app.scss';
 import './app-media.scss';
-import '../../style/button.scss'
+import '../../style/button.scss';
+import '../../style/button-media.scss';
 
 import Header from '../header/Header';
 import World from '../world/World';
 import Achieve from '../achieve/Achieve';
+import Grow from '../grow/Grow';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Header/>
       <World/>
       <Achieve/>
+      <Grow/>
     </>
   );
 }
---
+++ src/components/grow/Grow.js
@@ -0,0 +1,20 @@
+import './grow.scss';
+import './grow-media.scss';
+
+const Grow = () => {
+    return (
+        <div className="grow">
+            <div className="container">
+                <div className="grow__wrapper">
+                    <div className="grow__subtitle">Let's Grow Together</div>
+                    <div className="grow__title">We	turn creative ideas into your business.</div>
+                    <div className="grow__descr">Lorem ipsum dolor sit am et, consec tetur adipi scing elit. Sed sodales enim ut rhoncus lorem ipsum ese terds. Lorem ipsum dolor sit am et, consec tetur adipi scing elit.</div>
+                </div>
+
+                <button className="grow__button btn-shadow">Read more</button>
+            </div>
+        </div>
+    )
+}
+
+export default Grow;
